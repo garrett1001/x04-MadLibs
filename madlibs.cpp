817fe107b12a4bc6324d8e55a3f73dcfebf163e1
garrett1001 <gwelton@mail.csuchico.edu>
2018-02-01 17:09:31 -0800

    Completed

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,26 @@
-//Authors: 
+//Authors: Garrett Welton
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main()
 {
+
+string Body;
+char Symb;
+int Numb;
+
+cout<<"Enter a body part"<<endl;
+cin>>Body;
+
+cout<<"Enter a letter"<<endl;
+cin>>Symb;
+
+cout<<"Enter a number"<<endl;
+cin>>Numb;
+
+cout<<"Bob fell on his "<<Body<<"\nevery time he walked down the\nstares. A Doctor named "<<Symb<<"\nsaid he can only fall 3 days a\nweek in order to live. He fell\n"<<Numb<<" times that week."<<endl;
 
   return 0;
 }
